d309ec83b9ead7e5d752a142dcd8cceea75ef5f3
Ashik11898 <mohamed.ashik@broadbil.com>
2024-12-18 11:24:36 +0400

    cors url updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: [
-        'http://localhost:5000', 'http://localhost:5173', 'https://timesheet-app-eight.vercel.app', 'http://192.168.151.103:5173','https://timesheet-app-server.vercel.app'], // Update with your frontend origin
+        'http://localhost:5000', 'http://localhost:5173', 'https://timesheet-app-eight.vercel.app', 'http://192.168.151.103:5173','https://timesheet-app-server.vercel.app','https://main.dn4a98mcjtbb9.amplifyapp.com'], // Update with your frontend origin
     credentials: true // Allow cookies to be sent
 }));
 
